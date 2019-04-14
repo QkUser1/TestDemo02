a227a571826ed57860187cc3ba844e46462a3e16
Manoj Patil <Q3479@Corp.qualitykiosk.com>
2019-04-14 13:16:35 +0530

    first commit

--- src/com/qklab/monitoring/Testjob01.java
+++ src/com/qklab/monitoring/Testjob01.java
@@ -3,5 +3,6 @@ package com.qklab.monitoring;
 public class Testjob01 {
 	public static void main(String[] args) {
 		System.out.println("Hi this is test message");
+		System.out.println("Hi I M sachin and this is test message");
 	}
 }
